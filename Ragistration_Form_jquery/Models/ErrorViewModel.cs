a418e23a14a519e8e1ed9e339847816c7da9f140
GovindAnncode <govindanncode2000@gmail.com>
2024-01-03 08:28:10 +0530

    Add project files.

---
+++ Ragistration_Form_jquery/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Ragistration_Form_jquery.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
